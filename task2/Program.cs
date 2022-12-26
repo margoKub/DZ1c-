6150d4041d1dc729b833a770ebddb280e09346af
MargaritaKub <lora313@yandex.ru>
2022-12-26 23:50:50 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два неодинаковых целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine ($"{a}=max, {b}=min");
+}
+else
+{
+    Console.WriteLine ($"{b}=max, {a}=min");
+}
